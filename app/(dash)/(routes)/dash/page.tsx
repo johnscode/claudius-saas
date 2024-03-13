495e1b53defe2eb196c3f60aef9f0c8f92e9eb70
John F <john@johnscode.com>
2024-03-13 13:57:25 -0500

    fork from next14-ai-saas

--- app/(dash)/(routes)/dash/page.tsx
+++ app/(dash)/(routes)/dash/page.tsx
@@ -11,57 +11,13 @@ import {Card} from "@/components/ui/card";
 import {cn} from "@/lib/utils";
 import {tools} from "@/constants";
 
-// const tools = [
-//     {
-//         label: 'Conversation',
-//         icon: MessageSquare,
-//         href: '/conversation',
-//         color: "text-violet-500",
-//         bgColor: "bg-violet-500/10",
-//     },
-//     {
-//         label: 'Music Generation',
-//         icon: Music,
-//         href: '/music',
-//         color: "text-emerald-500",
-//         bgColor: "bg-emerald-500/10",
-//     },
-//     {
-//         label: 'Image Generation',
-//         icon: ImageIcon,
-//         color: "text-pink-700",
-//         bgColor: "bg-pink-700/10",
-//         href: '/image',
-//     },
-//     {
-//         label: 'Video Generation',
-//         icon: VideoIcon,
-//         color: "text-orange-700",
-//         bgColor: "bg-orange-700/10",
-//         href: '/video',
-//     },
-//     {
-//         label: 'Code Generation',
-//         icon: Code,
-//         color: "text-green-700",
-//         bgColor: "bg-green-700/10",
-//         href: '/code',
-//     },
-// ];
 
 const DashPage = () => {
     const router = useRouter();
     return (
         <main>
-            <p className="text-3xl text-blue-500"> Dashboard</p>
-            <div className="mb-8 space-y-4">
-                <h2 className="text-2xl md:text-4xl font-bold text-center">
-                    Eve
-                </h2>
-                <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-                    How can Eve help you?
-                </p>
-            </div>
+            <h2 className="text-3xl text-blue-700 text-center font-bold"> Dashboard</h2>
+            <div className="mb-8 space-y-4">   </div>
             <div className="px-4 md:px-20 lg:px-32 space-y-4">
                 {tools.map((tool) => (
                     <Card onClick={() => router.push(tool.href)} key={tool.href} className="p-4 border-black/5 flex items-center
